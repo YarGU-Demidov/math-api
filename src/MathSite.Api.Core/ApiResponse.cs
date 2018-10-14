928b0ef96f7908cc4fb6abeeb44b15748144d5e5
Andrey Mokeev <andrey@mokeev1995.ru>
2018-10-15 00:46:57 +0300

    добавил детали ошибки для ErrorApiResponse

--- src/MathSite.Api.Core/ApiResponse.cs
+++ src/MathSite.Api.Core/ApiResponse.cs
@@ -4,10 +4,11 @@ namespace MathSite.Api.Core
 {
     public sealed class ErrorApiResponse<T> : ApiResponse<T>
     {
-        public ErrorApiResponse(string reason) : base(default)
+        public ErrorApiResponse(string reason, string details = null) : base(default)
         {
             Status = ApiStatuses.Error;
             Reason = reason;
+            Details = details;
         }
     }
 
@@ -27,7 +28,7 @@ namespace MathSite.Api.Core
             Data = data;
         }
 
-        [JsonProperty("data")]
+        [JsonProperty("data", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         public T Data { get; set; }
     }
 
@@ -35,13 +36,16 @@ namespace MathSite.Api.Core
     {
         [JsonProperty("status")]
         public string Status { get; protected set; }
-
+        
         [JsonProperty("reason", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         public string Reason { get; protected set; }
+        
+        [JsonProperty("details", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        public string Details { get; protected set; }
 
         public bool HasError()
         {
-            return !string.IsNullOrWhiteSpace(Status) && Status.ToLowerInvariant() == "error";
+            return !string.IsNullOrWhiteSpace(Status) && Status.ToLowerInvariant() == ApiStatuses.Error;
         }
     }
 }
